8ef29241abfae3e7d82c04a26e7277d2d2c4ebf6
Corey Howell <cjthowell@gmail.com>
2015-11-29 15:06:02 -0500

    testing mysql connection

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ var Hapi = require('hapi');
 
 var port = process.env.PORT || 3000;
 
+var mysql = require('mysql'),
+    sup;
+
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
+
+connection.connect(function(err) {
+    if (err) {
+        sup = "oh no :(";
+    } else {
+        sup = "oh yeah!";
+    }
+});
+
 var server = new Hapi.Server();
 server.connection({ port: port });
 
@@ -9,7 +27,7 @@ server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        reply('Hello, world!');
+        reply('Hello, world!' + '<BR><BR>' + sup);
     }
 });
 
